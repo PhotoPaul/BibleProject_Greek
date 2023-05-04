ebb5ef0de18e6b1757b7e9fa0346b1b7ecb8abb9
PhotoPaul <PhotoPaul@gmail.com>
2023-05-04 09:47:52 +0300

    Removed unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bibleproject_greek/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
